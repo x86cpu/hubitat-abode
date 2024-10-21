b809f8f34a2d7f9b8836dbfedc927988f811c163
Eric Meddaugh <eric@meddaughs.com>
2024-10-21 13:03:39 -0400

    Add Doorbell driver

---
+++ drivers/AbodeDoorbell.groovy
@@ -0,0 +1,40 @@
+/*
+ * Abode Alarm
+ *
+ * Copyright 2020 Jo Rhett.  All Rights Reserved
+ * Copyright 2024 Eric Meddaugh.  All Rights Reserved
+ * Started from Hubitat example driver code https://github.com/hubitat/HubitatPublic/tree/master/examples/drivers
+ * Implementation inspired by https://github.com/MisterWil/abodepy
+ *
+ *  Licensed under the Apache License, Version 2.0 -- details in the LICENSE file in this repo
+ *
+ *  Original code: https://github.com/jorhett/hubitat-abode
+ *
+ */
+
+ metadata {
+  definition (
+    name: 'Abode Alarm Doorbell',
+    namespace: 'x86cpu',
+    author: 'Eric Meddaugh',
+    importUrl: 'https://raw.githubusercontent.com/x86cpu/hubitat-abode/master/drivers/AbodeDoorbell.groovy',
+  ) {
+    capability "Actuator"
+    attribute 'button', "ENUM", ["pushed", "released"]
+  }
+ }
+
+// Hubitat standard methods
+def installed() {
+  log.debug 'installed'
+}
+
+def uninstalled() {
+  log.debug 'installed'
+}
+
+def updated() {
+}
+
+def refresh() {
+}
